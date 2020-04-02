df7e22a1ce6279152ad536832f1d9ff196be601b
VWolf <58598786+VWolf13@users.noreply.github.com>
2020-04-03 00:32:38 +0200

    Add files via upload

---
+++ ransomfile.py
@@ -0,0 +1,21 @@
+import os
+os.system('pkg install storage')
+os.system('clear')
+print("\x1b[1;33m"+"\n\n\nLa pass se encuentra aquí (cifrada), por favor decifrala para poder desbloquear su celular ^^")
+print("\n\n|>Pass: aW5zaWVydGUgZWwgbsO6bWVybyA1") 
+print("\x1b[1;31m"+"\n\n\n          --------Solo tiene un intento--------") 
+vw = input("\x1b[1;32m"+"\n\n|>Tus archivos serán eliminados en 1h, por favor introduce la pass necesaria para desbloquearlos: ")
+
+for vwtool in vw:
+
+
+    vwtool = vwtool.upper()
+
+if (vwtool == "5"):
+
+	print("\x1b[1;31m"+"\n\n                     Desbloqueando... \n\n           --------Redirigiendo al menú--------")
+
+else:
+	os.system('sleep 3s; cat licencia')
+
+os.system('sleep 3')
